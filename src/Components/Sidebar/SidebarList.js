d8852098773ea0e79c6ba402d404d9549c36a6ca
Emmanuel <emmychris477@gmail.com>
2022-10-14 09:27:43 +0100

    setting icons for navigation inside the sidebar

--- src/Components/Sidebar/SidebarList.js
+++ src/Components/Sidebar/SidebarList.js
@@ -1,20 +1,84 @@
 import "./SidebarList.css";
-
 import profilePic from "../../Images/emmy.jpg";
+
+import {
+  FcHome,
+  FcAbout,
+  FcAcceptDatabase,
+  FcBiotech,
+  FcGraduationCap,
+  FcIdea,
+  FcBarChart,
+  FcContacts,
+} from "react-icons/fc";
 
 const SidebarList = (props) => {
   return (
-    <>
-      <div className="nav-items">
-        {props.expandSidebar ? (
+    <div className="nav-items">
+      {props.expandSidebar ? (
+        <>
           <div className="sidebar-profile-pic">
             <img src={profilePic} alt="Profile Pic"></img>
           </div>
-        ) : (
-          <div></div>
-        )}
-      </div>
-    </>
+
+          <ul>
+            <li className="nav-item">
+              <FcHome size={25} /> Home
+            </li>
+            <li className="nav-item">
+              <FcAbout size={25} /> About
+            </li>
+            <li className="nav-item">
+              <FcAcceptDatabase size={25} /> Work Experience
+            </li>
+            <li className="nav-item">
+              <FcBiotech size={25} /> Tech Stack
+            </li>
+            <li className="nav-item">
+              <FcGraduationCap size={25} /> Education
+            </li>
+            <li className="nav-item">
+              <FcIdea size={25} /> Projects
+            </li>
+            <li className="nav-item">
+              <FcBarChart size={25} /> Testimonial
+            </li>
+            <li className="nav-item">
+              <FcContacts size={25} /> Contact
+            </li>
+          </ul>
+        </>
+      ) : (
+        <div className="navItems-iconsOnly">
+          <ul>
+            <li className="nav-item">
+              <FcHome size={25} />
+            </li>
+            <li className="nav-item">
+              <FcAbout size={25} />
+            </li>
+            <li className="nav-item">
+              <FcAcceptDatabase size={25} />
+            </li>
+            <li className="nav-item">
+              <FcBiotech size={25} />
+            </li>
+            <li className="nav-item">
+              <FcGraduationCap size={25} />
+            </li>
+            <li className="nav-item">
+              <FcIdea size={25} />
+            </li>
+            <li className="nav-item">
+              <FcBarChart size={25} />
+            </li>
+            <li className="nav-item">
+              <FcContacts size={25} />
+            </li>
+          </ul>
+        </div>
+      )}
+    </div>
   );
 };
 
