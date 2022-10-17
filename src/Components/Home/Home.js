7085225815617841be59cfa335d5f81a884b5db2
Emmanuel <emmychris477@gmail.com>
2022-10-17 21:10:46 +0100

    home comp. with typewritter effect implement

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,7 +1,27 @@
 import "./Home.css";
+import Typewritter from "typewriter-effect";
 
 const Home = () => {
-  return <div>Home Section Here</div>;
+  return (
+    <div className="container-fluid home">
+      <div className="container home-content">
+        <h1>Hi, I'm a </h1>
+        <h2>
+          <Typewritter
+            options={{
+              strings: [
+                "Software Engineer",
+                "Tech Enthusiast",
+                "Resourceful Youth",
+              ],
+              autoStart: true,
+              loop: true,
+            }}
+          />
+        </h2>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
