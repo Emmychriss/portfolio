240757c18e4d836f6c6fd60a0c7f90382d8f77ae
Emmanuel <emmychris477@gmail.com>
2022-10-18 22:43:17 +0100

    declared action btns and import CV

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,5 +1,6 @@
 import "./Home.css";
 import Typewritter from "typewriter-effect";
+import myCV from "../../assets/CV/Emmanuel_Nduka_CV.pdf"
 
 const Home = () => {
   return (
@@ -19,6 +20,14 @@ const Home = () => {
             }}
           />
         </h2>
+        <div className="action-button">
+          <div className="hire-me-btn">Hire Me</div>
+          <div className="getResume-btn">
+            <a href={myCV} download="Nduka_Emma_CV">
+              Get My Resume
+            </a>
+          </div>
+        </div>
       </div>
     </div>
   );
