1a2b6eb6b3122ae3f30feb0f5cfea3ea5fd7774c
Emmanuel <emmychris477@gmail.com>
2022-10-19 20:30:07 +0100

    rendered about in app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 
 import Sidebar from "../src/Components/Sidebar/Sidebar";
+import About from "./Components/About/About";
 
 function App() {
   return (
     <div className="App">
       <Sidebar />
+      <About />
     </div>
   );
 }
