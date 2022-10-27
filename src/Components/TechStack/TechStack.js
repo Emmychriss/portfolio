7f3c8a1532f4e2eaaaf89e0e16c6ad7327ee90b6
Emmanuel <emmychris477@gmail.com>
2022-10-27 16:09:42 +0100

    rendered list of tech stacks with arr. of object(data API)

--- src/Components/TechStack/TechStack.js
+++ src/Components/TechStack/TechStack.js
@@ -1,7 +1,60 @@
 import "./TechStack.css";
 
 const TechStack = () => {
-  return <div className="container">This is the tech stack component</div>;
+  const data = [
+    {
+      name: "Front-end developer",
+    },
+    {
+      name: "React js",
+    },
+    {
+      name: "Javascript(vanilla)",
+    },
+    {
+      name: "Typescript",
+    },
+    {
+      name: "SQL",
+    },
+    {
+      name: "mySQL server",
+    },
+    {
+      name: "Node js",
+    },
+    {
+      name: "Next js",
+    },
+    {
+      name: "Express js",
+    },
+    {
+      name: "Mongo Db",
+    },
+  ];
+
+  return (
+    <div className="container techStack-section">
+      <div className="techStack-title">
+        <h5>Tech Stack</h5>
+        <span className="line"></span>
+      </div>
+
+      <div className="row">
+        {data.map((item, i, arr) => {
+          return (
+            <div
+              className="col-xl-4 col-lg-4 col-md-6 col-sm-12"
+              key={`${item.name}${Math.random()}`}
+            >
+              <div className="techStack-content">{<p>{item.name}</p>}</div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default TechStack;
