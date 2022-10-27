13e18d9dc268a56fb47a720348ee0e1d9f1d8e81
Emmanuel <emmychris477@gmail.com>
2022-10-27 12:37:39 +0100

    rendered tech stack in app comp.

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import "./App.css";
 
 import Sidebar from "../src/Components/Sidebar/Sidebar";
 import About from "./Components/About/About";
+import TechStack from "./Components/TechStack/TechStack";
 
 function App() {
   return (
     <div className="App">
       <Sidebar />
       <About />
+      <TechStack /> 
     </div>
   );
 }
