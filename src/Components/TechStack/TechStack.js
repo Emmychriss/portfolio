c827f869c5fe4fd684c70ff13e7f57ad3025b848
Emmanuel <emmychris477@gmail.com>
2022-10-28 11:17:42 +0100

    added tech stack number on each item

--- src/Components/TechStack/TechStack.js
+++ src/Components/TechStack/TechStack.js
@@ -4,33 +4,43 @@ const TechStack = () => {
   const data = [
     {
       name: "Front-end developer",
+      color: "#F0B041",
     },
     {
       name: "React js",
+      color: "#68D68D",
     },
     {
       name: "Javascript(vanilla)",
+      color: "#7D3C98",
     },
     {
       name: "Typescript",
+      color: "#76D7C4",
     },
     {
       name: "SQL",
+      color: "#6E2C0D",
     },
     {
       name: "mySQL server",
+      color: "#F1C410",
     },
     {
       name: "Node js",
+      color: "#CD6155",
     },
     {
       name: "Next js",
+      color: "#DC7633",
     },
     {
       name: "Express js",
+      color: "#3F80BA",
     },
     {
       name: "Mongo Db",
+      color: "#306B3B",
     },
   ];
 
@@ -42,13 +52,18 @@ const TechStack = () => {
       </div>
 
       <div className="row">
-        {data.map((item, i, arr) => {
+        {data.map((item, index, arr) => {
           return (
-            <div
-              className="col-xl-4 col-lg-4 col-md-6 col-sm-12"
-              key={`${item.name}${Math.random()}`}
-            >
-              <div className="techStack-content">{<p>{item.name}</p>}</div>
+            <div className="col-xl-4 col-lg-4 col-md-6 col-sm-12" key={index}>
+              <div className="techStack-content">
+                <span
+                  className="techStack-number"
+                  style={{ backgroundColor: data[index].color }}
+                >
+                  {index + 1}
+                </span>
+                {<p>{item.name}</p>}
+              </div>
             </div>
           );
         })}
