2760e5206d52264cbea973ffdc53a84e3ce81abd
Emmanuel <emmychris477@gmail.com>
2022-10-30 22:19:58 +0100

    added another typewriter string

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -14,6 +14,7 @@ const Home = () => {
                 "Software Engineer",
                 "Tech Enthusiast",
                 "Resourceful Youth",
+                "Problem Solver",
               ],
               autoStart: true,
               loop: true,
