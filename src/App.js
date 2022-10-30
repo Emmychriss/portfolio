0df85d0aad449530d76e0115a613cfaddf185840
Emmanuel <emmychris477@gmail.com>
2022-10-30 22:15:01 +0100

    rendered project in app

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import "./App.css";
 import Sidebar from "../src/Components/Sidebar/Sidebar";
 import About from "./Components/About/About";
 import TechStack from "./Components/TechStack/TechStack";
+import Project from "./Components/Projects/Project";
 
 function App() {
   return (
     <div className="App">
       <Sidebar />
       <About />
-      <TechStack /> 
+      <TechStack />
+      <Project />
     </div>
   );
 }
