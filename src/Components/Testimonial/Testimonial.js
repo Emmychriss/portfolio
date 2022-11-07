b76e17ee168a3620e35951d5dd6d04d62b5e6d38
Emmanuel <emmychris477@gmail.com>
2022-11-07 19:59:56 +0100

    testimonial comp.

---
+++ src/Components/Testimonial/Testimonial.js
@@ -0,0 +1,12 @@
+import "./Testimonial.css";
+
+const Testimonial = () => {
+  return <div className="container testimonial-section">
+    <div className="testimonial-title">
+        <h5>Testimonial</h5>
+        <span className="line"></span>
+      </div>
+  </div>;
+};
+
+export default Testimonial;
