c0becea60f020e8e36beb90722e89fa32b14f2d3
Emmanuel <emmychris477@gmail.com>
2022-11-09 19:39:52 +0100

    added slick slider settings and rendered slider by data API loop

--- src/Components/Testimonial/Testimonial.js
+++ src/Components/Testimonial/Testimonial.js
@@ -62,11 +62,62 @@ const Testimonial = () => {
     },
   ];
 
+  var settings = {
+    dots: true,
+    infinite: false,
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    initialSlide: 0,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
+
   return (
     <div className="container testimonial-section">
       <div className="testimonial-title">
         <h5>Testimonial</h5>
         <span className="line"></span>
+      </div>
+
+      <div className="testimonial-slider">
+        <Slider {...settings}>
+          {data.map((item, index, arr) => {
+            return (
+              <div className="slider-content" key={index}>
+                <img
+                  src={item.image}
+                  alt="Testimonial Pic"
+                  className="centerImage "
+                ></img>
+              </div>
+            );
+          })}
+        </Slider>
       </div>
     </div>
   );
