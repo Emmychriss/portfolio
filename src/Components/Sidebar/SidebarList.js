63d0262f23400a21651332302628163b5ec89479
Emmanuel <emmychris477@gmail.com>
2022-11-14 23:31:22 +0100

    react scroll using Link in action

--- src/Components/Sidebar/SidebarList.js
+++ src/Components/Sidebar/SidebarList.js
@@ -1,5 +1,6 @@
 import "./SidebarList.css";
 import profilePic from "../../assets/Images/emmy.jpg";
+import { Link } from "react-scroll";
 
 import {
   FcHome,
@@ -23,28 +24,92 @@ const SidebarList = (props) => {
 
           <ul>
             <li className="nav-item">
-              <FcHome size={25} /> Home
+              <Link
+                to="home"
+                spy={true}
+                smooth={true}
+                offset={-50}
+                duration={100}
+              >
+                <FcHome size={25} /> Home
+              </Link>
             </li>
             <li className="nav-item">
-              <FcAbout size={25} /> About
+              <Link
+                spy={true}
+                smooth={true}
+                offset={-50}
+                duration={100}
+                to="about"
+              >
+                <FcAbout size={25} /> About
+              </Link>
             </li>
             <li className="nav-item">
-              <FcAcceptDatabase size={25} /> Work Experience
+              <Link
+                spy={true}
+                smooth={true}
+                offset={-50}
+                duration={100}
+                to="workExperience"
+              >
+                <FcAcceptDatabase size={25} /> Work Experience
+              </Link>
             </li>
             <li className="nav-item">
-              <FcBiotech size={25} /> Tech Stack
+              <Link
+                spy={true}
+                smooth={true}
+                offset={-50}
+                duration={100}
+                to="techStack"
+              >
+                <FcBiotech size={25} /> Tech Stack
+              </Link>
             </li>
             <li className="nav-item">
-              <FcGraduationCap size={25} /> Education
+              <Link
+                spy={true}
+                smooth={true}
+                offset={-50}
+                duration={100}
+                to="education"
+              >
+                <FcGraduationCap size={25} /> Education
+              </Link>
             </li>
             <li className="nav-item">
-              <FcIdea size={25} /> Projects
+              <Link
+                spy={true}
+                smooth={true}
+                offset={-50}
+                duration={100}
+                to="projects"
+              >
+                <FcIdea size={25} /> Projects
+              </Link>
             </li>
             <li className="nav-item">
-              <FcBarChart size={25} /> Testimonial
+              <Link
+                spy={true}
+                smooth={true}
+                offset={-50}
+                duration={100}
+                to="testimonial"
+              >
+                <FcBarChart size={25} /> Testimonial
+              </Link>
             </li>
             <li className="nav-item">
-              <FcContacts size={25} /> Contact
+              <Link
+                spy={true}
+                smooth={true}
+                offset={-100}
+                duration={100}
+                to="contact"
+              >
+                <FcContacts size={25} /> Contact
+              </Link>
             </li>
           </ul>
         </>
